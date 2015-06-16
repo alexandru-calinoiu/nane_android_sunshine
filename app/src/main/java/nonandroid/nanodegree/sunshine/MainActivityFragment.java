afc839a98639d70a556ae69dba0a7af3d26b192b
calin <calinoiu.alexandru@agilefreaks.com>
2015-06-16 12:13:43 +0300

    1.04 add dumy data

--- app/src/main/java/nonandroid/nanodegree/sunshine/MainActivityFragment.java
+++ app/src/main/java/nonandroid/nanodegree/sunshine/MainActivityFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * A placeholder fragment containing a simple view.
  */
@@ -17,6 +20,10 @@ public class MainActivityFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_main, container, false);
+    View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+    ArrayList<String> forecasts = new ArrayList<>(Arrays.asList("Today", "Tomorrow"));
+
+    return view;
   }
 }
