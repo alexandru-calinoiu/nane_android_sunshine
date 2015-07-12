d357dc015e2d6456b9c6b62413958152b930618a
calin <calinoiu.alexandru@agilefreaks.com>
2015-07-12 11:11:47 +0300

    adds life cycle logic

--- app/src/main/java/nonandroid/nanodegree/sunshine/MainActivity.java
+++ app/src/main/java/nonandroid/nanodegree/sunshine/MainActivity.java
@@ -5,17 +5,56 @@ import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+  private final String LOG_TAG = MainActivity.class.getSimpleName();
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    Log.i(LOG_TAG, "onCreate");
   }
 
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    Log.i(LOG_TAG, "onPause");
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+
+    Log.i(LOG_TAG, "onStop");
+  }
+
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    Log.i(LOG_TAG, "onResume");
+  }
+
+  @Override
+  protected void onStart() {
+    super.onStart();
+
+    Log.i(LOG_TAG, "onStart");
+  }
+
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+
+    Log.i(LOG_TAG, "onDestroy");
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
