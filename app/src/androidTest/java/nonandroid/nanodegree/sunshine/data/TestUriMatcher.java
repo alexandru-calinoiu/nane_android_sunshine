9957f35d921a067f0087df82f78c6e97df338be5
calin <calinoiu.alexandru@agilefreaks.com>
2015-07-12 14:56:22 +0300

    fixes  spec for test uri matcher

--- app/src/androidTest/java/nonandroid/nanodegree/sunshine/data/TestUriMatcher.java
+++ app/src/androidTest/java/nonandroid/nanodegree/sunshine/data/TestUriMatcher.java
@@ -15,6 +15,7 @@
  */
 package nonandroid.nanodegree.sunshine.data;
 
+import android.content.UriMatcher;
 import android.net.Uri;
 import android.test.AndroidTestCase;
 
@@ -25,32 +26,32 @@ import android.test.AndroidTestCase;
     a nice compromise between data hiding and testability.
  */
 public class TestUriMatcher extends AndroidTestCase {
-    private static final String LOCATION_QUERY = "London, UK";
-    private static final long TEST_DATE = 1419033600L;  // December 20th, 2014
-    private static final long TEST_LOCATION_ID = 10L;
+  private static final String LOCATION_QUERY = "London, UK";
+  private static final long TEST_DATE = 1419033600L;  // December 20th, 2014
+  private static final long TEST_LOCATION_ID = 10L;
 
-    // content://nonandroid.nanodegree.sunshine/weather"
-    private static final Uri TEST_WEATHER_DIR = WeatherContract.WeatherEntry.CONTENT_URI;
-    private static final Uri TEST_WEATHER_WITH_LOCATION_DIR = WeatherContract.WeatherEntry.buildWeatherLocation(LOCATION_QUERY);
-    private static final Uri TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR = WeatherContract.WeatherEntry.buildWeatherLocationWithDate(LOCATION_QUERY, TEST_DATE);
-    // content://nonandroid.nanodegree.sunshine/location"
-    private static final Uri TEST_LOCATION_DIR = WeatherContract.LocationEntry.CONTENT_URI;
+  // content://nonandroid.nanodegree.sunshine/weather"
+  private static final Uri TEST_WEATHER_DIR = WeatherContract.WeatherEntry.CONTENT_URI;
+  private static final Uri TEST_WEATHER_WITH_LOCATION_DIR = WeatherContract.WeatherEntry.buildWeatherLocation(LOCATION_QUERY);
+  private static final Uri TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR = WeatherContract.WeatherEntry.buildWeatherLocationWithDate(LOCATION_QUERY, TEST_DATE);
+  // content://nonandroid.nanodegree.sunshine/location"
+  private static final Uri TEST_LOCATION_DIR = WeatherContract.LocationEntry.CONTENT_URI;
 
-    /*
-        Students: This function tests that your UriMatcher returns the correct integer value
-        for each of the Uri types that our ContentProvider can handle.  Uncomment this when you are
-        ready to test your UriMatcher.
-     */
-//    public void testUriMatcher() {
-//        UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
-//
-//        assertEquals("Error: The WEATHER URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_DIR), WeatherProvider.WEATHER);
-//        assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR), WeatherProvider.WEATHER_WITH_LOCATION);
-//        assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
-//                testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR), WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE);
-//        assertEquals("Error: The LOCATION URI was matched incorrectly.",
-//                testMatcher.match(TEST_LOCATION_DIR), WeatherProvider.LOCATION);
-//    }
+  /*
+      Students: This function tests that your UriMatcher returns the correct integer value
+      for each of the Uri types that our ContentProvider can handle.  Uncomment this when you are
+      ready to test your UriMatcher.
+   */
+  public void testUriMatcher() {
+    UriMatcher testMatcher = WeatherProvider.buildUriMatcher();
+
+    assertEquals("Error: The WEATHER URI was matched incorrectly.",
+        WeatherProvider.WEATHER, testMatcher.match(TEST_WEATHER_DIR));
+    assertEquals("Error: The WEATHER WITH LOCATION URI was matched incorrectly.",
+        WeatherProvider.WEATHER_WITH_LOCATION, testMatcher.match(TEST_WEATHER_WITH_LOCATION_DIR));
+    assertEquals("Error: The WEATHER WITH LOCATION AND DATE URI was matched incorrectly.",
+        WeatherProvider.WEATHER_WITH_LOCATION_AND_DATE, testMatcher.match(TEST_WEATHER_WITH_LOCATION_AND_DATE_DIR));
+    assertEquals("Error: The LOCATION URI was matched incorrectly.",
+        WeatherProvider.LOCATION, testMatcher.match(TEST_LOCATION_DIR));
+  }
 }
